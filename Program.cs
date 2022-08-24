70145305ca64fab4564be614948d62cb472a1e6c
Alumno <Alumno@L15-2543313>
2022-08-24 11:38:14 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace expendedorabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            expendedora miexpendedora = new expendedora();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ expendedora.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading;
+
+namespace expendedorabase
+{
+    class expendedora
+    {
+        string marca;
+        byte temperatura;
+        string codigo;
+        float precio;
+        public expendedora()
+        {
+            saludo();
+            marca = "AMS";
+            Console.WriteLine("marca : " + marca);
+            controlartiempodisplay();
+            temperatura = 14;
+            Console.WriteLine("la temperatura es de :" + temperatura+ "°C");
+            controlartiempodisplay();
+            codigo = mostrarcodigodeproductos();
+            controlartiempodisplay();
+            mostrarprecio(codigo);
+            
+        }
+        void controlartiempodisplay()
+        {
+            Thread.Sleep(3000);
+            Console.Clear();
+        }
+        void saludo()
+        {
+            Console.WriteLine("bienvenido a esta maquina");
+        }
+
+        string mostrarcodigodeproductos()
+        {
+            Console.WriteLine("A1 : kinder delice \t B2 : takis\t C3 : Panditas");
+            Console.WriteLine("ingresa el codigo deseado ");
+            string codigodo = Console.ReadLine(); // codigo es variable local
+            return codigo;
+        }
+        void mostrarprecio(string codigo)
+        {
+            switch (codigo)
+            {
+                case "A1":
+                    Console.WriteLine("el precio es de : {0}", precio);
+                    break;
+                case "B2":
+                    Console.WriteLine("el precio es de : {0}", precio);
+                    break;
+                case "C3":
+                    Console.WriteLine("el precio es de : {0}", precio);
+                
+                    break;
+                
+
+
+
+            }
+        }
+    }
+}
